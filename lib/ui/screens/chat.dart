2d2465917734b9e8ee86bdece1ef8c6c8180f9ff
Aditya Agrawal <adityaisking012@gmail.com>
2020-05-30 13:55:59 -0500

    chat

---
+++ lib/ui/screens/chat.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:socialchatbotapp/global.dart';
+import 'package:socialchatbotapp/ui/widgets/widgets.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  bool _showBottom = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black54),
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            MyCircleAvatar(
+              imgUrl: friendsList[0]['imgUrl'],
+            ),
+            SizedBox(width: 15),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Cybdom Tech",
+                  style: Theme.of(context).textTheme.subhead,
+                  overflow: TextOverflow.clip,
+                ),
+                Text(
+                  "Online",
+                  style: Theme.of(context).textTheme.subtitle.apply(
+                        color: myGreen,
+                      ),
+                )
+              ],
+            )
+          ],
+        ),
+
+      ),
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(35.0),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 3),
+                          blurRadius: 5,
+                          color: Colors.grey)
+                    ],
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "               🤖 AI                 or                    🙎‍ HUMAN️  ",
+                              border: InputBorder.none),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.all(15),
+                    itemCount: messages.length,
+                    itemBuilder: (ctx, i) {
+                      if (messages[i]['status'] == MessageType.received) {
+                        return ReceivedMessagesWidget(i: i);
+                      } else {
+                        return SentMessageWidget(i: i);
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  height: 61,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(35.0),
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset(0, 3),
+                                  blurRadius: 5,
+                                  color: Colors.grey)
+                            ],
+                          ),
+                          child: Row(
+                            children: <Widget>[
+                              IconButton(
+                                  icon: Icon(Icons.face), onPressed: () {}),
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Type Something...",
+                                      border: InputBorder.none),
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+List<IconData> icons = [
+];
