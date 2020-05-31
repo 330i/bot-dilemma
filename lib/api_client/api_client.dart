b7f2238055fadbfbf948ca1d78642e24ef6dfaad
PrasannS <singhal.prasann07@gmail.com>
2020-05-30 21:36:46 -0500

    some new changes

--- lib/api_client/api_client.dart
+++ lib/api_client/api_client.dart
@@ -7,6 +7,7 @@ Future<String> fetchChatBotResult(String val) async {
   print(val);
   String url = 'http://192.168.0.59:8080/interact?text='+val;
   Response response = await get(url);
+  print(response);
   // sample info available in response
   print(response.body);
   String chatResult = json.decode(response.body)['text'];
